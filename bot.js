2e47e83746919ed422e28da4a7ed9e57ca28fa9a
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-05-07 18:50:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setGame(`on 15 servers | *help |
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
 });
 client.on('message', message => {
      if (message.content === ".servers") {
